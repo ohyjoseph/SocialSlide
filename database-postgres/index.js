0e62dba1eeec205a6ce2a8be81add21d080e2f34
Joseph Oh <ohyjoseph@gmail.com>
2018-05-25 15:37:58 -0500

    Adding methods to database-postgres file

--- database-postgres/index.js
+++ database-postgres/index.js
@@ -7,30 +7,65 @@ client.connect((err) => {
   if (err) {
     return console.error('ERROR connecting to postgres:', err);
   }
-  console.log('CONNECTED to postgres slidedb!');
+  return console.log('CONNECTED to postgres slidedb!');
 });
 
-client.query('SELECT * FROM tblUsers', (err, result) => {
-  if (err) {
-    return console.error('ERROR running query:', err);
+ function selectUser (params, cb) {
+  client.query('SELECT * FROM tblUsers WHERE username = $1', [params.username], (err, result) => {
+    if (err) {
+      return console.error('ERROR running query:', err);
+    }
+    if (result.rowCount < 1) {
+      return console.error('User does NOT exist');
+    }
+    cb(result.rows);
+  });
+}
+
+function checkLogin (params, cb) {
+  client.query('SELECT * FROM tblUsers WHERE username = $1 AND password = $2', [params.username, params.password], (err, result) => {
+    if (err) {
+      return console.error('ERROR running query:', err);
+    }
+    if (result.rowCount < 1) {
+      return console.error('NOT a correct username and password');
+    }
+    cb(result.rows);
+  });
+}
+
+function insertUser (params, cb) {
+  if (!params.avatarUrl || params.avatarUrl === '') {
+    params.avatarUrl = null;
   }
-  console.log(result.rows);
-});
+  client.query('INSERT INTO tblUsers (username, password, avatarUrl) VALUES ($1, $2, $3)', [params.username, params.password, params.avatarUrl], (err, result) => {
+    if (err) {
+      return console.error('ERROR inserting user:', err);
+    }
+    cb(result);
+  });
+}
 
-// let client = pg.connection(conString, (err, client, done) => {
-//   if (err) {
-//     return console.error('ERROR connecting to Slide database:', err);
-//   }
-//   return client;
-// });
+function insertMessage (params, cb) {
+  client.query('INSERT INTO tblDms (sender, receiver, message) VALUES ($1, $2, $3)', [params.sender, params.receiver, params.message], (err, result) => {
+    if (err) {
+      return console.error('ERROR inserting message:', err);
+    }
+    cb(result);
+  });
+}
 
-// var selectAll = function(callback) {
-//   connection.query('SELECT * FROM items', function(err, results, fields) {
-//     if(err) {
-//       callback(err, null);
-//     } else {
-//       callback(null, results);
-//     }
-//   });
-// };
+function insertFriendRequest (params, cb) {
+  client.query('INSERT INTO tblFriends (sender, receiver) VALUES ($1, $2)', [params.sender, params.receiver], (err, result) => {
+    if (err) {
+      return console.error('ERROR inserting friend request:', err);
+    }
+    cb(result);
+  });
+}
+
+// selectUser ({username:'test'}, (data) => console.log(data));
+// checkLogin({username: 'test8', password: 'test5'}, (data) => console.log(data));
+// insertMessage({sender:'test', receiver:'test3', message: 'pizza tastes really good?'}, (data) => console.log(data));
+insertFriendRequest({sender:'test3', receiver:'test2'}, (data) => console.log(data));
 module.exports.client = client;
--- database-postgres/schema.sql
+++ database-postgres/schema.sql
@@ -1,5 +1,5 @@
 CREATE SCHEMA IF NOT EXISTS slidedb AUTHORIZATION oh;
 
-    CREATE TABLE slidedb.tblUsers (username varchar(50) primary key, password varchar(50) NOT NULL, avatarUrl varchar(2083));
-    CREATE TABLE slidedb.tblFriends (sender varchar(50), receiver varchar(50), wasAccepted boolean, CONSTRAINT pk PRIMARY KEY (sender,receiver));
-    CREATE TABLE slidedb.tblDms (dmId serial primary key, sender varchar(50) NOT NULL, receiver varchar(50) NOT NULL, message text NOT NULL);
+    CREATE TABLE slidedb.tblUsers (username varchar(50) primary key, password varchar(50) NOT NULL, avatarUrl varchar(2083), createdAt timestamp NOT NULL default NOW());
+    CREATE TABLE slidedb.tblFriends (sender varchar(50), receiver varchar(50), wasAccepted boolean, CONSTRAINT pk PRIMARY KEY (sender,receiver), createdAt timestamp NOT NULL default NOW());
+    CREATE TABLE slidedb.tblDms (dmId serial primary key, sender varchar(50) NOT NULL, receiver varchar(50) NOT NULL, message text NOT NULL, createdAt timestamp NOT NULL default NOW());
