d8f8062e2e25c63a77e62e89fbbffa56735a5f87
Joseph Oh <ohyjoseph@gmail.com>
2018-05-26 11:10:16 -0500

    fixed heroku postgres url

--- database-postgres/index.js
+++ database-postgres/index.js
@@ -1,7 +1,7 @@
 const pg = require('pg');
 //$ service postgresql start 
 
-var conString = process.env.PORT || 'postgres://oh:2112@localhost/slidedb';
+var conString = process.env.DATABASE_URL || 'postgres://oh:2112@localhost/slidedb';
 
 let client = new pg.Client(conString);
 client.connect((err) => {
