0d9dc24d8a226c53e9056adeba18bca711815a0b
Joseph Oh <ohyjoseph@gmail.com>
2018-05-26 11:47:14 -0500

    testing insert into db on heroku postgres

--- server/index.js
+++ server/index.js
@@ -53,4 +53,4 @@ app.listen(process.env.PORT || 3000, function() {
   console.log('listening on port 3000!');
 });
 
-db.insertUser({username: 'tesddt', password: 'tesddt'}, (data) => console.log(data));
+db.insertUser({username: 'joseph', password: 'joseph'}, (data) => console.log(data));
