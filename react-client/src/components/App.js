afefde0df68d04d9605ee8e0ae6b76304beddd05
Joseph Oh <ohyjoseph@gmail.com>
2018-09-13 18:29:02 -0500

    Added index as root. Connected redux

--- react-client/src/components/App.js
+++ react-client/src/components/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import $ from 'jquery';
 import { BrowserRouter as Router, Link, Redirect} from 'react-router-dom';
 import Route from 'react-router-dom/Route';
 
-import Navbar from './components/Navbar.jsx';
-import Home from './components/Home.jsx';
-import FriendRequestList from './components/FriendRequestList.jsx';
-import FriendList from './components/FriendList.jsx';
-import DmList from './components/DmList.jsx';
-import Login from './components/Login.jsx';
-import Signup from './components/Signup.jsx';
+import Navbar from './Navbar.jsx';
+import Home from './Home.jsx';
+import DmList from './DmList.jsx';
+import Login from './Login.jsx';
+import Signup from './Signup.jsx';
 //withRouter 
 //react history
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -20,31 +17,31 @@ class App extends React.Component {
     console.log(`LOGGED IN: ${window.localStorage.getItem('loggedIn')} USERNAME: ${window.localStorage.getItem('username')} AVATAR: ${window.localStorage.getItem('avatarUrl')}`);
   }
 
-  render () {
+  render() {
     return (
       <Router>
         <div className='container'>
           <div className='app'>
             <div className='navbar'>
-              <Navbar/>
+              <Navbar />
             </div>
-              {/* <Route path='/' exact strict component = {Home}/> */}
+            {/* <Route path='/' exact strict component = {Home}/> */}
             <div className='home'>
               <Route path='/' exact strict render={() => (
                 window.localStorage.getItem('loggedIn') === 'true' ? (<Home />) : (<Redirect to='/login' />)
-              )}/>
+              )} />
             </div>
-              {/* <Route path='/login' exact strict component = {Login}/> */}
+            {/* <Route path='/login' exact strict component = {Login}/> */}
             <div className='login'>
               <Route path='/login' exact strict render={() => (
                 window.localStorage.getItem('loggedIn') !== 'true' ? (<div><Login /> <Signup /></div>) : (<Redirect to='/' />)
-              )}/>
+              )} />
             </div>
             <div className='dm'>
               {/* <Route path='/dm' exact strict component={DmList} /> */}
               <Route path='/dm' exact strict render={() => (
-                window.localStorage.getItem('loggedIn') === 'true' ? (<div><DmList location={this.props.location}/></div>) : (<Redirect to='/' />)
-              )}/>
+                window.localStorage.getItem('loggedIn') === 'true' ? (<div><DmList location={this.props.location} /></div>) : (<Redirect to='/' />)
+              )} />
             </div>
           </div>
         </div>
@@ -53,4 +50,4 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+export default App;
